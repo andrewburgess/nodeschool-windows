27786f98810ce67eab75b246be49d556d0da36d7
Andrew Burgess <andrew@andrewburgess.io>
2015-06-13 17:17:34 -0700

    Initial proof of concept
    Demonstrate running NodeJS code in a Windows application.

---
+++ NodeSchool/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using EdgeJs;
+
+namespace NodeSchool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private bool codeFocused;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var version = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            Title += " v" + version;
+        }
+
+        private void Code_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (!codeFocused)
+            {
+                // Clear out the default text for now
+                Code.Text = "";
+                codeFocused = true;
+            }
+        }
+
+        private void Code_LostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Code.Text))
+            {
+                Code.Text = "Type code here...";
+                codeFocused = false;
+            }
+        }
+
+        private void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConsoleOutput.Text += "\n> Executing code...\n";
+            var script = ComposeScript(Code.Text);
+            Task.Run(() => Run(script)).Wait();
+        }
+
+        private async void Run(string script)
+        {
+            var consoleLog = (Func<object, Task<object>>)(async (message) =>
+            {
+                var str = Encoding.UTF8.GetString((byte[])message);
+                ConsoleOutput.Dispatcher.BeginInvoke((Action) (() => ConsoleOutput.Text += str));
+                return null;
+            });
+
+            var runFunc = Edge.Func(script);
+
+            await runFunc(new {
+                log = consoleLog
+            });
+
+            ConsoleOutput.Dispatcher.BeginInvoke((Action) (() => ConsoleOutput.Text += "\n"));
+        }
+
+        private string ComposeScript(string code)
+        {
+            const string template = @"
+                return function (options, done) {
+                    // Windows GUI Applications mess up the STDIN/STDOUT streams to the point
+                    // where Node will get all sorts of mad. What we're doing here is telling
+                    // Node to use our MagicStream which will basically send output up to our
+                    // handler to execute.
+                    (function () {
+                        var stream = require('stream');
+                        var MagicStream = function (o) {
+                            stream.Writable.call(this);
+                            this._write = function (c, e, cb) {
+                                options.log(c); 
+                                cb && cb();
+                            };
+                        }
+                        require('util').inherits(MagicStream, stream.Writable);
+                        var nullStream = new MagicStream();
+                        process.__defineGetter__('stdout', function () { return nullStream; });
+                        process.__defineGetter__('stderr', function () { return nullStream; });
+                    })();
+                    
+                    {{CODE}}
+
+                    done();
+                }
+            ";
+
+            var script = template.Replace("{{CODE}}", code);
+            return script;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConsoleOutput.Text = "";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # nodeschool-windows
 Crazy proof of concept for a Windows application that could be used to onboard users for NodeSchool
+
+Highly experimental
+
+# Overview
+Conceived as part of a conversation at NodeConf regarding getting more people into programming via
+NodeSchool. One hurdle outlined was the difficulty for Windows users to get up and running.
+
+Using the equally insane [Edge.js](https://github.com/tjanczuk/edge) project, this shows a proof of
+concept of potentially creating a standalone Windows application that can introduce a user to Node
+and let them run actual code on their own system.
+
+# How To Run
+This was built using the VS 2013 Community Edition, so at least that. Also needs the .NET 4.5 framework
+
+Before running, make sure to install the dependencies using NuGet.
+
+After that, just click the Run button (I hope).
+
+# Vision
+There may be a way to let Windows users run NodeSchool workshops without needing to install Node
+itself or worry about touching the command line until they have gained enough experience to be
+able to tackle those issues.
